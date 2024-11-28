3af94e71344b1e7a5ea56c53db97b01d3b9cc472
xscrypto001 <ikinyapeter93@gmail.com>
2024-11-28 14:27:37 +0300

    final front commit

--- med-frontend/vite.config.js
+++ med-frontend/vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically update the service worker
+      manifest: {
+        name: "OrgPro CRM",
+        short_name: "OrgPro",
+        description: "Turn Leads to Gold.",
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/signin',
+        icons: [
+          {
+            src: '/assets/icons/logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+  },
+});
