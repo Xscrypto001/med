e54104c84a77273be774ef7a1bf6bb49748b17a1
stanondieki <ondiekistanley21@gmail.com>
2024-11-28 14:35:38 +0300

    vite project initializa

--- med-frontend/src/App.jsx
+++ med-frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/login';
+import HealthcareFundraiserDashboard from './pages/Dashboard';
+import './index.css';
+import Signup from './components/register';
+//import Dashboard from './components/Dashboard';
+import PayoutPage from './pages/Payout';
+import MissionPage from './pages/info';
+import HealthCampaignDetailPage from './pages/payment_page';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+     <Router>
+       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard/donations" element={<  HealthcareFundraiserDashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/payouts" element={<PayoutPage />} />
+        <Route path="/donate" element={<HealthCampaignDetailPage />} />
+        <Route path="/info" element={<MissionPage />} />
 
-export default App
+
+
+      {/*   <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        {/*<Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/payout" element={<Payout />} />*/}
+      </Routes> 
+     </Router>
+  );
+};
+
+export default App;
--- med-frontend/vite.config.js
+++ med-frontend/vite.config.js
@@ -1,34 +1,10 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate', // Automatically update the service worker
-      manifest: {
-        name: "OrgPro CRM",
-        short_name: "OrgPro",
-        description: "Turn Leads to Gold.",
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/signin',
-        icons: [
-          {
-            src: '/assets/icons/logo-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/icons/logo-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+   
   ],
   server: {
     host: true,
